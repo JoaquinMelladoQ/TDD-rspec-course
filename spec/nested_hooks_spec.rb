9fc5ed852608beb44107254bc31997c00477b18a
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-11-08 14:48:02 -0300

    added a context inner and outer before and after

--- spec/nested_hooks_spec.rb
+++ spec/nested_hooks_spec.rb
@@ -1,13 +1,30 @@
 RSpec.describe 'nested hooks' do
     before(:context) do
-        puts 'Before context'
+        puts 'Outer before context'
     end
 
     before(:example) do
-        puts 'Before example'
+        puts 'outer before example'
     end
 
     it 'does basic math' do
         expect(1 + 1).to eq(2) 
+    end
+    
+    context 'with condition A' do
+        before(:context) do
+            puts 'Inner before context'
+        end
+    
+        before(:example) do
+            puts 'Inner before example'
+        end
+
+        it 'does basic math' do
+           expect(1 + 1).to eq(2)   
+        end
+        it 'it does substraction as well' do
+           expect(3 - 2).to eq(1)   
+        end
     end
 end
