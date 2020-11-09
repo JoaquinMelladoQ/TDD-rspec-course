b19f1e0adbc76d604dcffebff3dbd76d15545c9d
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-11-09 17:09:25 -0300

    testig if its savig values from the call

--- spec/implicit_subject_spec.rb
+++ spec/implicit_subject_spec.rb
@@ -4,5 +4,7 @@ RSpec.describe Hash do
     it 'should start off empty' do
         puts subject.class 
         expect(subject.length).to eq(0)  
+        subject[:some_key] = "Some value"
+        expect(subject.length).to eq(1)  
     end
 end
