323ef3f374746cc6745842ddd2c7a0a8c343350f
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-11-11 16:47:10 -0300

    dry principles applied to

--- spec/shared_examples_spec.rb
+++ spec/shared_examples_spec.rb
@@ -1,19 +1,22 @@
-RSpec.describe Array do
-    subject { [1, 2, 3] } 
+RSpec.shared_examples 'a ruby object with three elements' do
     it 'returns the number of items' do
         expect(subject.length).to eq(3)
     end
 end
 
+RSpec.describe Array do
+    subject { [1, 2, 3] } 
+    include_examples 'a ruby object with three elements'
+end
+    
 RSpec.describe String do
     subject { 'abc' }
-    it 'returns the number of items' do
-        expect(subject.length).to eq(3)
-    end 
+    include_examples 'a ruby object with three elements'
 end
 
 RSpec.describe Hash do
-    subject { { a: 1, b: 2, c: 3} } 
+    subject { { a: 1, b: 2, c: 3 } } 
+    include_examples 'a ruby object with three elements'
 end
 
 class SausageLink
@@ -24,5 +27,6 @@ end
 
 RSpec.describe SausageLink do
     subject { described_class.new } 
+    include_examples 'a ruby object with three elements'
 end
 
