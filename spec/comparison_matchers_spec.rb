d6cf4f9b700ef60a74d9d644dc5f6e8b2328b213
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-01 15:05:19 -0300

    added a set of comparisons instead of place directly the object

--- spec/comparison_matchers_spec.rb
+++ spec/comparison_matchers_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe 'comparison matchers' do
         expect(1).to be >= 1
         
         expect(22).to be <= 100  
-        expect(22).to be <= 22  
+        expect(22).to be <= 22
+    end
+
+    describe 100 do
+        it { is_expected.to be > 90 }    
+        it { is_expected.to be >= 100 }    
+        it { is_expected.to be < 500 }    
+        it { is_expected.to be <= 100 }    
+        it { is_expected.not_to be > 105 }    
     end
 end
