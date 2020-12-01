cb76d414dd3defd9aa3d8bb5fb1d5835f858c686
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-01 18:52:23 -0300

    added a single line that do the same

--- spec/all_matcher_spec.rb
+++ spec/all_matcher_spec.rb
@@ -1,7 +1,9 @@
 RSpec.describe 'all matcher' do
     it 'allows for aggregate checks' do
-        [5, 7, 9].each do |val|
-            expect(val).to be_odd
-        end
+        # [5, 7, 9].each do |val|
+        #     expect(val).to be_odd
+        # end
+        expect([5, 7, 9]).to all(be_odd)  
+
     end
 end
