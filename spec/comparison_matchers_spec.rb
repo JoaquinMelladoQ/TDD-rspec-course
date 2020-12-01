48c63e59d3152dc67e15ac3b1cb5811c094c26d1
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-01 14:47:47 -0300

    added more operators

--- spec/comparison_matchers_spec.rb
+++ spec/comparison_matchers_spec.rb
@@ -1,5 +1,12 @@
 RSpec.describe 'comparison matchers' do
     it 'allows for comparison with built-in Ruby operator' do
         expect(10).to be > 5  
+        expect(8).to be < 15
+        
+        expect(1).to be >= -1  
+        expect(1).to be >= 1
+        
+        expect(22).to be <= 100  
+        expect(22).to be <= 22  
     end
 end
