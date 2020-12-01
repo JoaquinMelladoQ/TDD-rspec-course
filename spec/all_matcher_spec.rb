cd874b54a415277370856f046e4200770764fb60
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-01 19:02:14 -0300

    added a more succinted way

--- spec/all_matcher_spec.rb
+++ spec/all_matcher_spec.rb
@@ -8,6 +8,11 @@ RSpec.describe 'all matcher' do
         expect([[], [], []]).to all(be_empty)  
         expect([0, 0]).to all(be_zero)  
         expect([5, 7, 9]).to all(be < 10)
-
     end
+
+    describe [5, 7, 9] do
+        it { is_expected.to all(be_odd) }
+        it { is_expected.to all(be < 10) }
+    end
+    
 end
