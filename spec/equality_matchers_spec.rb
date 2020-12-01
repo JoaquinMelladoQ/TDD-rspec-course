82e832fb3b0baba07c38dd059da96e546818182f
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-01 12:17:53 -0300

    eq matcher was added

--- spec/equality_matchers_spec.rb
+++ spec/equality_matchers_spec.rb
@@ -1,4 +1,11 @@
 RSpec.describe 'equality-matchers' do
     let(:a) { 3.0 }
-    let(:b) { 3 }  
+    let(:b) { 3 }
+    
+    describe "eq matcher" do
+        it 'test for value and ignores type' do
+            
+        end
+    end
+    
 end
