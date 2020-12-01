f1105ef2121ed76c2f3d9a7bfebe211fa5f2fed5
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-01 18:58:43 -0300

    added more examples

--- spec/all_matcher_spec.rb
+++ spec/all_matcher_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe 'all matcher' do
         #     expect(val).to be_odd
         # end
         expect([5, 7, 9]).to all(be_odd)  
+        expect([4, 6, 8, 10]).to all(be_even)  
+        expect([[], [], []]).to all(be_empty)  
+        expect([0, 0]).to all(be_zero)  
+        expect([5, 7, 9]).to all(be < 10)
 
     end
 end
