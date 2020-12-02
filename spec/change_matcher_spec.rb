04559419b5083128ebac161cfca7a2345412b9c0
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-02 11:36:02 -0300

    added a more flexible way of doing the same

--- spec/change_matcher_spec.rb
+++ spec/change_matcher_spec.rb
@@ -1,7 +1,8 @@
 RSpec.describe 'change matcher' do
-    subject { [1, 2, 3] }
+    subject { [1, 2, 3, 4] }
     
     it 'checks that a method changes object state' do
-        expect { subject.push(4) }.to change { subject.length }.from(3).to(4)  
+        # expect { subject.push(4) }.to change { subject.length }.from(3).to(4)  
+        expect { subject.push(4) }.to change { subject.length }.by(1)  
     end
 end
