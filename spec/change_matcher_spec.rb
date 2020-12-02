5c0c967a0674585ec65be5a9c35dd775d42feb32
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-02 12:18:40 -0300

    added a test for negative arguments

--- spec/change_matcher_spec.rb
+++ spec/change_matcher_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe 'change matcher' do
         # expect { subject.push(4) }.to change { subject.length }.from(3).to(4)  
         expect { subject.push(4) }.to change { subject.length }.by(1)  
     end
+
+    it 'accepts negative arguments' do
+        expect { subject.pop }.to change { subject.length }.from(4).to(3)
+    end
 end
