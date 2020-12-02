d96a32b66e5ed6d00edd080e2e7fc8884ecab7c0
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-02 13:03:31 -0300

    added a end with test passed

--- spec/start_with_and_end_with_matchers_spec.rb
+++ spec/start_with_and_end_with_matchers_spec.rb
@@ -2,6 +2,7 @@ RSpec.describe 'start_with and end_with matchers' do
     describe 'caterpillar' do
         it 'should check for a substring at the beginning or end' do
             expect(subject).to start_with('cat')  
+            expect(subject).to end_with('pillar')  
         end
     end
     
