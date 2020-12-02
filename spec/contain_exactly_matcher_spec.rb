fafd23a0e44d53322d4ff309ca9135a73977c2fc
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-02 12:41:00 -0300

    added a more short way to do the same : all test passed

--- spec/contain_exactly_matcher_spec.rb
+++ spec/contain_exactly_matcher_spec.rb
@@ -8,7 +8,11 @@ RSpec.describe 'contain_exactly matcher' do
             expect(subject).to contain_exactly(2, 1, 3)
             
             # expect(subject).to contain_exactly(3, 1)  
-            expect(subject).to contain_exactly(3, 1, 2, 4)  
+            # expect(subject).to contain_exactly(3, 1, 2, 4)  
         end
+
+        it { is_expected.to contain_exactly(1, 2, 3) }
+        it { is_expected.to contain_exactly(3, 2, 1) }
+        it { is_expected.to contain_exactly(2, 1, 3) }
     end
 end
