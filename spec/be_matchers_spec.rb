3f707099179c25cc250c32d305f6727bd1ae71b4
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-01 22:43:45 -0300

    runned a test for falsiness and truthiness

---
+++ spec/be_matchers_spec.rb
@@ -0,0 +1,22 @@
+# Falsy values -- false, nil
+# Truthy values -- everything else
+
+RSpec.describe 'be matchers' do
+    it 'can test for truthiness' do
+        expect(true).to be_truthy  
+        expect('Hello').to be_truthy  
+        expect(5).to be_truthy  
+        expect(0).to be_truthy  
+        expect(-1).to be_truthy  
+        expect(3.14).to be_truthy  
+        expect([]).to be_truthy  
+        expect([1, 2]).to be_truthy  
+        expect({}).to be_truthy  
+        expect(:symbol).to be_truthy  
+    end
+
+    it 'can test for falsiness' do
+        expect(false).to be_falsy  
+        expect(nil).to be_falsy  
+    end
+end
