3baad3c8a399061d8071791603bd85ec89db516a
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-02 12:35:41 -0300

    added a contain exactly to check

--- spec/contain_exactly_matcher_spec.rb
+++ spec/contain_exactly_matcher_spec.rb
@@ -5,8 +5,9 @@ RSpec.describe 'contain_exactly matcher' do
         it 'should check for the presence of all elements' do
             expect(subject).to contain_exactly(1, 2, 3)  
             expect(subject).to contain_exactly(3, 2, 1)  
-            expect(subject).to contain_exactly(2, 1, 3)  
-            expect(subject).to contain_exactly(3, 1, 2)  
+            expect(subject).to contain_exactly(2, 1, 3)
+            
+            expect(subject).to contain_exactly(3, 1)  
         end
     end
 end
