9aac096e325f7ed1658e0f052a508de7de70cd9c
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-02 13:21:53 -0300

    evaluate an array with start with

--- spec/start_with_and_end_with_matchers_spec.rb
+++ spec/start_with_and_end_with_matchers_spec.rb
@@ -8,5 +8,12 @@ RSpec.describe 'start_with and end_with matchers' do
         it { is_expected.to start_with('cat') }
         it { is_expected.to end_with('pillar') }
     end
+
+    describe [:a, :b, :c, :d] do
+        it 'should check for elements at the beginning or end of the array' do
+            expect(subject).to start_with(:a) 
+        end
+    end
+    
     
 end
