696d3a0bd95c3a463ff3400e951e8d89b9bb366f
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-03 19:54:24 -0300

    respond to matcher with a givin symbol

---
+++ spec/respond_to_matcher_spec.rb
@@ -0,0 +1,25 @@
+class HotChocolate
+    def drink
+        'Delicious'
+    end
+
+    def discard
+        'PLOL!'
+    end
+
+    def purchase(number)
+        "Awesome, I just purchased #{number} more hot chocolate beverages!"
+    end
+end
+
+# class coffee
+#     def drink; end
+#     def discard; end 
+#     def purchase(number); end 
+# end
+
+RSpec.describe HotChocolate do
+    it 'confirms that an object can respond to a method' do
+        expect(subject).to respond_to(:drink) 
+    end
+end
