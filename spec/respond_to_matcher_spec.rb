87a5f5b13355301e65486d1781fc04cada16cf11
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-03 19:56:10 -0300

    added more and it worked, even with purchase

--- spec/respond_to_matcher_spec.rb
+++ spec/respond_to_matcher_spec.rb
@@ -21,5 +21,7 @@ end
 RSpec.describe HotChocolate do
     it 'confirms that an object can respond to a method' do
         expect(subject).to respond_to(:drink) 
+        expect(subject).to respond_to(:drink, :discard) 
+        expect(subject).to respond_to(:drink, :discard, :purchase) 
     end
 end
