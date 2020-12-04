41891d23add45b4763020f209dbc0bba13636a3f
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-04 09:29:10 -0300

    and was added to manipulate double test

--- spec/compound_expectations_spec.rb
+++ spec/compound_expectations_spec.rb
@@ -1,6 +1,8 @@
 RSpec.describe 25 do
     it 'can test for multiple matchers' do
-        expect(subject).to be_odd
-        expect(subject).to be > 20  
+        # expect(subject).to be_odd
+        # expect(subject).to be > 20
+        
+        expect(subject).to be_odd.and be > 20  
     end
 end
