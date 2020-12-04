24d01654fccb665b7c8ea34118a046acd77a7fdd
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-04 00:14:33 -0300

    expect not to be truthy example added

--- spec/not_to_spec.rb
+++ spec/not_to_spec.rb
@@ -2,6 +2,8 @@ RSpec.describe 'not_to method' do
     it 'checks for tho inverse of a matcher' do
         expect(5).not_to eq(10)
         expect([1, 2, 3]).not_to equal([1, 2, 3])
-        expect(10).not_to be_odd     
+        expect(10).not_to be_odd
+        
+        expect(nil).not_to be_truthy
     end
 end
