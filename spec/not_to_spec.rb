b62d2c13d9f17d092fe588d559559f94a83e603a
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-04 00:20:04 -0300

    added a expect for a string

--- spec/not_to_spec.rb
+++ spec/not_to_spec.rb
@@ -5,5 +5,8 @@ RSpec.describe 'not_to method' do
         expect(10).not_to be_odd
         
         expect(nil).not_to be_truthy
+
+        expect('Philadelphia').not_to start_with('car')  
+        expect('Philadelphia').not_to end_with('city')  
     end
 end
