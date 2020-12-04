f665213506195cbef04eed9609416d41cf60463d
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-04 09:35:23 -0300

    single line code was inserted

--- spec/compound_expectations_spec.rb
+++ spec/compound_expectations_spec.rb
@@ -13,4 +13,6 @@ RSpec.describe 'caterpillar' do
     it 'supports multiple matchers on a single line' do
         expect(subject).to start_with('cat').and end_with('pillar')  
     end
+
+    it { is_expected.to start_with('cat').and end_with('pillar') }
 end
