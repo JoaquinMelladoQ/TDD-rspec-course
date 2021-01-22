1af9e13584f3f381b7ae923f2d2dd1afcb2ab51b
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2021-01-21 23:31:17 -0300

    Double is mocking the result but it must return an array with those three elements

--- spec/matching_arguments_spec.rb
+++ spec/matching_arguments_spec.rb
@@ -1,9 +1,12 @@
-# EXAMPLES TO MOCK A CERTAIN BAHEVIOR WHEN USING ARR.FIRST(10000) WHILE IS EXPECTED TO BE AN ARRAY OF 3 ELEMENTS
+# EXAMPLES TO MOCK A CERTAIN BEHAVIOR WHEN USING ARR.FIRST(10000) WHILE IS EXPECTED TO BE AN ARRAY OF 3 ELEMENTS
 
 
 RSpec.describe "matching arguments" do
     it "can return different values depending on the arguments" do
         three_element_array = double # [1, 2, 3]
         allow(three_element_array).to receive(:first).with(no_args).and_return(1)
+        allow(three_element_array).to receive(:first).with(be >= 3).and_return([1, 2, 3])
+
+        # DOUBLE IS MOCKING THE BEHAVIOR
     end
 end
