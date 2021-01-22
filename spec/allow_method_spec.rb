33de31023b743656c7487917f05b38fd4a29e230
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2021-01-21 22:50:10 -0300

    Works, expecting to return 15

--- spec/allow_method_spec.rb
+++ spec/allow_method_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe "allow method review" do
     it "can customize return values for methods on doubles" do
         calculator = double
-        allow(calculator).to receive(:add)
-        expect(calculator.add).to be_nil
+        allow(calculator).to receive(:add).and_return(15)
+        expect(calculator.add).to eq(15)
     end
 end
