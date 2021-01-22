a21914917f96d22bb7492e0f7bffb996237cc64a
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2021-01-22 10:13:11 -0300

    Second example, using as_stubbed_const

--- spec/rspec/class_double_spec.rb
+++ spec/rspec/class_double_spec.rb
@@ -13,6 +13,7 @@ end
 
 RSpec.describe CardGame do
     it "can only implement clas methods that are defined on a class" do
-        class_double('Deck', build: ['Ace', 'Queen'])
+        deck_klass = class_double('Deck', build: ['Ace', 'Queen']).as_stubbed_const
+        subject.start
     end
 end
