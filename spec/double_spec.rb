7a43f8b75adfdbcaf89ada6ea6e91a5cab46a420
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2021-01-21 21:33:16 -0300

    Receive_messeges implemented in order to execute the same test

--- spec/double_spec.rb
+++ spec/double_spec.rb
@@ -5,8 +5,13 @@ RSpec.describe 'a random double' do
     #    expect(stuntman.fall_off_ladder).to eq('Ouch')
     #    expect(stuntman.light_on_fire).to eq(true)
 
+        # stuntman = double("Mr. Danger")
+        # allow(stuntman).to receive(:fall_off_ladder).and_return('Ouch')
+        # expect(stuntman.fall_off_ladder).to eq('Ouch')
+
         stuntman = double("Mr. Danger")
-        allow(stuntman).to receive(:fall_off_ladder).and_return('Ouch')
+        allow(stuntman).to receive_messages(fall_off_ladder: 'Ouch', light_on_fire: true)
         expect(stuntman.fall_off_ladder).to eq('Ouch')
+        expect(stuntman.light_on_fire).to eq(true)
     end
 end
