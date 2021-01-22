ed965cd1720ecf43c43b879d7fb29890126e8002
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2021-01-21 22:53:14 -0300

    It will always return 15: add dosnt do anything

--- spec/allow_method_spec.rb
+++ spec/allow_method_spec.rb
@@ -3,5 +3,8 @@ RSpec.describe "allow method review" do
         calculator = double
         allow(calculator).to receive(:add).and_return(15)
         expect(calculator.add).to eq(15)
+        expect(calculator.add("it dosnt matter what you put here")).to eq(15)
+        expect(calculator.add(2, 3, 4)).to eq(15)
+        expect(calculator.add("it will always return 15 because add is empty")).to eq(15)
     end
 end
