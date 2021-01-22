b31073725bcd1d39fb1e7bc00bdef67c7830a09f
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2021-01-21 22:44:44 -0300

    Testing if methods are called once or twice

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -52,10 +52,16 @@ RSpec.describe Movie do
 
     describe "#start_shooting method" do
         it "expect an actor to do 3 actions" do
-            expect(stuntman).to receive(:ready?)
-            expect(stuntman).to receive(:act)
-            expect(stuntman).to receive(:fall_of_ladder)
-            expect(stuntman).to receive(:light_on_fire) 
+            # expect(stuntman).to receive(:ready?)
+            # expect(stuntman).to receive(:act)
+            # expect(stuntman).to receive(:fall_of_ladder)
+            # expect(stuntman).to receive(:light_on_fire) 
+
+            # expect(stuntman).to receive(:light_on_fire).once
+            # expect(stuntman).to receive(:light_on_fire).exactly(1).times  
+            expect(stuntman).to receive(:light_on_fire).at_most(1).times
+            expect(stuntman).to receive(:act).twice
+            # YOU CAN ADD SAME SYNTAX USED ABOVE WITH light_on_fire
             subject.start_shooting
         end
     end
